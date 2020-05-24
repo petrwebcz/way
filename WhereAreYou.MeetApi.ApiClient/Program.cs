b10df3399841ce0323bd22c47eb899a77381747e
Petr Svoboda <petr.svoboda@petrweb.cz>
2020-05-24 08:47:13 +0200

    add meet api client

---
+++ WhereAreYou.MeetApi.ApiClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhereAreYou.MeetApi.ApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
