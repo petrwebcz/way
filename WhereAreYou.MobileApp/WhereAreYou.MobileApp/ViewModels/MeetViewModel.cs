f23da947e72a615f03104b1a78244667886cf860
Petr Svoboda <petr.svoboda@petrweb.cz>
2020-06-28 11:37:01 +0200

    wip meet view

--- WhereAreYou.MobileApp/WhereAreYou.MobileApp/ViewModels/MeetViewModel.cs
+++ WhereAreYou.MobileApp/WhereAreYou.MobileApp/ViewModels/MeetViewModel.cs
@@ -1,7 +1,19 @@
-﻿namespace WhereAreYou.MobileApp.ViewModels
+﻿using System;
+using WhereAreYou.Core.Responses;
+
+namespace WhereAreYou.MobileApp.ViewModels
 {
     public class MeetViewModel : BaseViewModel
     {
+        public MeetViewModel()
+        {
+        }
 
+        public MeetViewModel(MeetResponse meetResponse)
+        {
+            MeetResponse = meetResponse ?? throw new ArgumentNullException(nameof(meetResponse));
+        }
+
+        public MeetResponse MeetResponse { get; }
     }
 }
