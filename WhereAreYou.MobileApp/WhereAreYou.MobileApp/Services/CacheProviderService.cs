44fe94029500fdf06a79516431923fc236b7aaf7
Petr Svoboda <petr.svoboda@petrweb.cz>
2020-07-31 16:49:06 +0200

    add cacheproviderservice to whereareyou.mobileapp

---
+++ WhereAreYou.MobileApp/WhereAreYou.MobileApp/Services/CacheProviderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace WhereAreYou.MobileApp.Services
+{
+    public class CacheProviderService : ICacheProviderService
+    {
+        private readonly IMemoryCache _cache;
+
+        public CacheProviderService()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions() { });
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            _cache.Set(key, value, DateTime.MaxValue); //TODO: Move to constants.
+        }
+
+        public T Get<T>(string key)
+        {
+            if (_cache.TryGetValue(key, out T value))
+                return value;
+            else
+                return default(T);
+        }
+    }
+}
---
+++ WhereAreYou.MobileApp/WhereAreYou.MobileApp/Services/ICacheProviderService.cs
@@ -0,0 +1,8 @@
+﻿namespace WhereAreYou.MobileApp.Services
+{
+    public interface ICacheProviderService
+    {
+        T Get<T>(string key);
+        void Set<T>(string key, T value);
+    }
+}
