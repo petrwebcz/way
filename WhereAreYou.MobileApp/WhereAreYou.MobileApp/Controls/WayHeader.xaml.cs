fa07c708fdb9d29a60d59f65859a4d19e8e26690
Petr Svoboda <petr.svoboda@petrweb.cz>
2020-08-06 12:04:47 +0200

    add way header in whereareyou.mobileapp

---
+++ WhereAreYou.MobileApp/WhereAreYou.MobileApp/Controls/WayHeader.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhereAreYou.MobileApp.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WayHeader : ContentView
+    {
+        public WayHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
